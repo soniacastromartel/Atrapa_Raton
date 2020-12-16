485792e2bcfba4bc8489be889b1a1de5f1738dd6
Sonia Castro <soniacastromartel@gmail.com>
2020-12-16 13:06:25 +0100

    Agregar archivos de proyecto.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.raton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // raton
+            // 
+            this.raton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.raton.Location = new System.Drawing.Point(244, 190);
+            this.raton.Name = "raton";
+            this.raton.Size = new System.Drawing.Size(124, 59);
+            this.raton.TabIndex = 0;
+            this.raton.Text = "Ratón";
+            this.raton.UseVisualStyleBackColor = true;            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(584, 441);
+            this.Controls.Add(this.raton);
+            this.Name = "Form1";
+            this.Text = "Atrapa al ratón!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button raton;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            raton.MouseMove += Raton_MouseMove;
+        }
+
+        private void Raton_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random rnd = new Random();
+            int x = (int)(this.ClientRectangle.Right * rnd.NextDouble()) - 30;
+            int y = (int)(this.ClientRectangle.Bottom * rnd.NextDouble()) - 10;
+            raton.Location = new Point(x, y);
+            
+
+        }
+
+        
+    }
+}
